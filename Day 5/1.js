9472f19979fa317d6b2b14aa937c355f7b000636
Jean Carlos <jeancarlos_maia@hotmail.com>
2019-12-05 16:59:50 -0300

    day 5

---
+++ Day 5/1.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+
+const data = fs.readFileSync("./input1.txt", "utf8");
+
+const formatInput = data => {
+  return data.split(",").map(Number);
+};
+
+let input = formatInput(data);
+
+let i = 0;
+
+while (i < input.length) {
+  const opcode = input[i];
+
+  if (opcode === 99) {
+    process.exit();
+  }
+
+  const DE = opcode % 100;
+  const C = Math.floor((opcode / 100) % 10);
+  const B = Math.floor((opcode / 1000) % 10);
+  const A = Math.floor((opcode / 10000) % 10);
+
+  const num1 = C === 0 ? input[input[i + 1]] : input[i + 1];
+  const num2 = B === 0 ? input[input[i + 2]] : input[i + 2];
+  const position = A === 0 ? input[i + 3] : i + 3;
+
+  if (DE === 1) input[position] = num1 + num2;
+  else if (DE === 2) input[position] = num1 * num2;
+  else if (DE === 3) input[input[i + 1]] = 1;
+  else console.log(input[input[i + 1]]);
+
+  DE <= 2 ? (i += 4) : (i += 2);
+}
---
+++ Day 5/2.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+
+const input = fs
+  .readFileSync("./input2.txt", "utf8")
+  .split(",")
+  .map(Number);
+
+let i = 0;
+
+while (i < input.length) {
+  const opcode = input[i];
+
+  const DE = opcode % 100;
+  const C = Math.floor((opcode / 100) % 10);
+  const B = Math.floor((opcode / 1000) % 10);
+  const A = Math.floor((opcode / 10000) % 10);
+
+  const num1 = C === 0 ? input[input[i + 1]] : input[i + 1];
+  const num2 = B === 0 ? input[input[i + 2]] : input[i + 2];
+  const position = A === 0 ? input[i + 3] : i + 3;
+
+  switch (DE) {
+    case 1:
+      input[position] = num1 + num2;
+      i += 4;
+      break;
+    case 2:
+      input[position] = num1 * num2;
+      i += 4;
+      break;
+    case 3:
+      input[input[i + 1]] = 5;
+      i += 2;
+      break;
+    case 4:
+      console.log(input[input[i + 1]]);
+      i += 2;
+      break;
+    case 5:
+      if (num1 !== 0) i = num2;
+      break;
+    case 6:
+      if (num1 === 0) i = num2;
+      break;
+    case 7:
+      input[position] = num1 < num2 ? 1 : 0;
+      break;
+    case 8:
+      input[position] = num1 === num2 ? 1 : 0;
+      break;
+    case 99:
+      process.exit();
+    default:
+      throw new Error("invalid opcode");
+  }
+}
